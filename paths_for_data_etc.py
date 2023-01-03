e72f464a092a56991673409bc3da761f6e17f48f
leo-lisahealth <leo@lisahealth.com>
2023-01-03 12:18:44 +0100

    working - need to fix requirements.txt

--- paths_for_data_etc.py
+++ paths_for_data_etc.py
@@ -7,29 +7,13 @@ import sys
 from pathlib import Path
 
 
+ROOT_DIR = Path(__file__).resolve().parent
 
+RAW_DATA_DIR = ROOT_DIR / "data" / "raw_data"
+age_raw_data_path = RAW_DATA_DIR / "raw_age_data.csv"
+height_raw_data_path = RAW_DATA_DIR / "raw_height_data.csv"
 
+CLEAN_DATA_DIR = ROOT_DIR / "data" / "cleaned_data"
+age_clean_data_path = CLEAN_DATA_DIR / "clean_age_data.csv"
+height_clean_data_path = CLEAN_DATA_DIR / "clean_height_data.csv"
 
-ROOT_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(str(ROOT_DIR))
-
-
- # RAW DATA paths
-RAW_DATA_DIR = ROOT_DIR.joinpath(r"data\raw_data").resolve()
-sys.path.append(str(RAW_DATA_DIR))
-#age
-age_raw_data = "raw_age_data.csv"
-age_raw_data_path = RAW_DATA_DIR.joinpath(age_raw_data).resolve()
-#height 
-height_raw_data = "raw_height_data.csv"
-height_raw_data_path = RAW_DATA_DIR.joinpath(height_raw_data).resolve()
-
- #CLEAN DATA paths
-CLEAN_DATA_DIR = ROOT_DIR.joinpath(r"data\cleaned_data").resolve()
-sys.path.append(str(CLEAN_DATA_DIR))
-#age
-age_clean_data = "clean_age_data.csv"
-age_clean_data_path = CLEAN_DATA_DIR.joinpath(age_clean_data).resolve()
-#height 
-height_clean_data = "clean_height_data.csv"
-height_clean_data_path = CLEAN_DATA_DIR.joinpath(height_clean_data).resolve()
